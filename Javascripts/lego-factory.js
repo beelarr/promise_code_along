48719c7142177f9b5eac8d324180da0a9b737b77
Bryon Larrance <beelarr@Bryons-MacBook-Pro.local>
2017-08-08 10:49:23 -0500

    Finished code along

--- Javascripts/lego-factory.js
+++ Javascripts/lego-factory.js
@@ -1,15 +1,28 @@
 var Legos = ((originalLegos) => {
 
+    let legoItems = []
+    let parseData = (data) => {
+        data.LegoColorss.forEach((element) =>{
+            legoItems.push(element)
+        })
+        return legoItems
+    }
+
+
     originalLegos.loadLegos = () => {
         return new Promise((resolve, reject) =>{
             let request = new XMLHttpRequest();
             request.onload = function () {
                 if (request.status === 200){
                     let data = JSON.parse(request.responseText);
-                    console.log(data)
+                    // console.log(data)
+                    resolve(parseData(data))
+                } else {
+                    reject(new Error('Error', request.statusText))
                 }
             }
             request.open('GET', './lego-colors.json')
+            request.send()
         })
     };
 
--- Javascripts/main.js
+++ Javascripts/main.js
@@ -13,10 +13,41 @@ greetingPromise();
 console.log('Hello');
 
 
-let colorPromise = Legos.loadLegos()
-    .then((resolve) => {
+let showItems = (legosData) => {
+    let legoDisplay = document.querySelector('#lego-display')
 
+    legosData.forEach((lego) => {
+        let legoBlock = buildLego(lego);
+        legoDisplay.innerHTML += legoBlock
+    })
+}
+
+let buildLego = (lego) => {
+    //building a string to create the visual display
+
+    //each separated by comma
+    let block = "",
+        wrapper = `<section class="block-wrapper" style="border: 2px solid #000000; background-color:#${lego.ColorHex}">`,
+        title = `<h3>Name: ${lego.LegoName}</h3>`,
+        years = `<div class="block-years">Manufactured ${lego.YearFrom} - ${lego.YearTo}</div>`;
+    // image = `<div class="card-img" style="background-image: url('images/${car.image}')"></div>`,
+    let link = lego.ColorstreamLinkImage;
+    if (link){
+        block += `<a href="${link}">${wrapper + title + years}</section></a>`;
+    }else{
+        block += `${wrapper + title + years}</section>`;
+    }
+    return block;
+}
+
+
+let colorPromise = Legos.loadLegos()
+    .then(
+        (resolve) => {
+            let newItem = {LegoName: "Bryon's Block", ColorHex: "a3a3d1", YearFrom: "2009", YearTo: "Present"}
+            resolve.push(newItem);
+            showItems(resolve)
         },
         (reject) =>{
             console.error(reject)
-        })
+        });
